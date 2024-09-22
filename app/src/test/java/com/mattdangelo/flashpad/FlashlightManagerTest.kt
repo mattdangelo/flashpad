d0d6946fd1c7241215adbb6649568c5638d66a35
Astradus <matthew.dangelo6@gmail.com>
2024-09-22 12:26:34 +0930

    Add test, bump deps

---
+++ app/src/test/java/com/mattdangelo/flashpad/FlashlightManagerTest.kt
@@ -0,0 +1,19 @@
+package com.mattdangelo.flashpad
+import android.content.pm.PackageManager
+import android.app.Application
+import org.junit.Test
+import org.junit.Assert.*
+import org.mockito.Mockito.*
+class FlashlightManagerTest {
+//    @Test
+//    fun `raises an exception on init when device does not have a camera flash`() {
+//        val application = mock(Application::class.java)
+//        val packageManager = mock(PackageManager::class.java)
+//        // Expect a device without a flash returns false for PackageManager.FEATURE_CAMERA_FLASH
+//        `when`(application.packageManager).thenReturn(packageManager)
+//        `when`(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)).thenReturn(false)
+//        assertThrows(UnsupportedOperationException::class.java) {
+//            FlashlightManager(application)
+//        }
+//    }
+}
