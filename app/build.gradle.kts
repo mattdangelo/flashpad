fbb993e8b3c8c7ca1cf452ebb1c4be11efe25fd2
Astradus <matthew.dangelo6@gmail.com>
2024-09-22 12:41:53 +0930

    Fix test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/test/java/com/mattdangelo/flashpad/FlashlightManagerTest.kt
+++ app/src/test/java/com/mattdangelo/flashpad/FlashlightManagerTest.kt
@@ -4,16 +4,17 @@ import android.app.Application
 import org.junit.Test
 import org.junit.Assert.*
 import org.mockito.Mockito.*
+
 class FlashlightManagerTest {
-//    @Test
-//    fun `raises an exception on init when device does not have a camera flash`() {
-//        val application = mock(Application::class.java)
-//        val packageManager = mock(PackageManager::class.java)
-//        // Expect a device without a flash returns false for PackageManager.FEATURE_CAMERA_FLASH
-//        `when`(application.packageManager).thenReturn(packageManager)
-//        `when`(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)).thenReturn(false)
-//        assertThrows(UnsupportedOperationException::class.java) {
-//            FlashlightManager(application)
-//        }
-//    }
+    @Test
+    fun `raises an exception on init when device does not have a camera flash`() {
+        val application = mock(Application::class.java)
+        val packageManager = mock(PackageManager::class.java)
+        // Expect a device without a flash returns false for PackageManager.FEATURE_CAMERA_FLASH
+        `when`(application.packageManager).thenReturn(packageManager)
+        `when`(packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)).thenReturn(false)
+        assertThrows(UnsupportedOperationException::class.java) {
+            FlashlightManager.getInstance(application)
+        }
+    }
 }
