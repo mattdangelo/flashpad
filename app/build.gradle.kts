c8953133931ccf221110f531b122d2e4152721bc
Astradus <matthew.dangelo6@gmail.com>
2025-03-23 17:06:54 +1030

    Update compile SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mattdangelo.flashpad"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mattdangelo.flashpad"
